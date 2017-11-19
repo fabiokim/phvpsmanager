da22d253241877f55fa1b9d74e9e83eb4ff34b1f
fabiokim <30272541+fabiokim@users.noreply.github.com>
2017-11-19 19:46:40 -0200

    Add files via upload

--- vpsmanager/scripts/ajuda.sh
+++ vpsmanager/scripts/ajuda.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-tput setaf 7 ; tput setab 4 ; tput bold ; printf '%30s%s%-10s\n' "Comandos disponíveis:" ; tput sgr0 ; echo ""
-tput setaf 2 ; tput bold ; printf '%s' "addhost " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 3 ; echo "Adicionar domínio 'host' a lista de domínios permitidos no Proxy Squid" ; echo ""
-tput setaf 2 ; tput bold ; printf '%s' "alterarlimite " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 3 ; echo "Alterar o número máximo de conexões simultâneas permitidas para um usuário" ; echo ""
-tput setaf 2 ; tput bold ; printf '%s' "alterarsenha " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 3 ; echo "Alterar a senha de um usuário" ; echo "" ;
-tput setaf 2 ; tput bold ; printf '%s' "criarusuario " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 3 ; echo "Criar usuário SSH sem acesso ao terminal e com data de expiração" ; echo ""
-tput setaf 2 ; tput bold ; printf '%s' "delhost " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 3 ; echo "Remover domínio 'host' da lista de domínios permitidos no Proxy Squid" ; echo ""
-tput setaf 2 ; tput bold ; printf '%s' "expcleaner " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 3 ; echo "Remover usuários SSH expirados" ; echo ""
-tput setaf 2 ; tput bold ; printf '%s' "mudardata " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 3 ; echo "Mudar a data de expiração de um usuário" ; echo ""
-tput setaf 2 ; tput bold ; printf '%s' "remover " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 3 ; echo "Remover um usuário SSH" ; echo ""
-tput setaf 2 ; tput bold ; printf '%s' "sshlimiter " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 3 ; echo "Limitador de conexões SSH simultâneas (deve ser executado em uma sessão screen)" ; echo ""
-tput setaf 2 ; tput bold ; printf '%s' "sshmonitor " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 3 ; echo "Verifica o número de conexão simutânea de cada usuário" ; echo ""
-tput sgr0
+tput setaf 4 ; tput setab 8 ; tput bold ; printf '%30s%s%-10s\n' "*******************************************" ; tput sgr0 ;
+tput setaf 4 ; tput setab 8 ; tput bold ; printf '%30s%s%-10s\n' "*   #### MENU OTIMIZADO @SUPORTEIMC ####   *" ; tput sgr0 ; 
+tput setaf 4 ; tput setab 8 ; tput bold ; printf '%30s%s%-10s\n' "*******************************************" ; tput sgr0 ; echo ""
+tput setaf 1 ; tput bold ; printf '%s' "[1] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "Criar usuario SSH" ; 
+tput setaf 1 ; tput bold ; printf '%s' "[2] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "Alterar a senha de um usuario" ; 
+tput setaf 1 ; tput bold ; printf '%s' "[3] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "Alterar o numero de conexoes simultaneas permitidas para um usuario" ; 
+tput setaf 1 ; tput bold ; printf '%s' "[4] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "Mudar a data de expiracao de um usuario" ; 
+tput setaf 1 ; tput bold ; printf '%s' "[5] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "Remover um usuario SSH" ; 
+tput setaf 1 ; tput bold ; printf '%s' "[6] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "Remover usuarios SSH expirados" ; 
+tput setaf 1 ; tput bold ; printf '%s' "[7] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "Limitador de conexoes SSH simultaneas" ; 
+tput setaf 1 ; tput bold ; printf '%s' "[8] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "SSH Monitor" ; 
+tput setaf 1 ; tput bold ; printf '%s' "[9] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "OpenVpn" ; 
+tput setaf 1 ; tput bold ; printf '%s' "[10] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "Otimizar" ;
+tput setaf 1 ; tput bold ; printf '%s' "[11] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "SpeedTest" ;
+tput setaf 1 ; tput bold ; printf '%s' "[12] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "Backup" ; 
+tput setaf 1 ; tput bold ; printf '%s' "[13] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "Informacoes do servidor (Uptime, RAM etc)" ; 
+tput setaf 1 ; tput bold ; printf '%s' "[14] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "Adicionar host no Proxy Squid" ; 
+tput setaf 1 ; tput bold ; printf '%s' "[15] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "Apagar host no Proxy Squid" ; 
+tput setaf 1 ; tput bold ; printf '%s' "[16] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "Instala o BadVPN" ; 
+tput setaf 1 ; tput bold ; printf '%s' "[17] " ; tput setaf 7 ; printf '%s' "- " ; tput setaf 7 ; echo "TCP Tweaker" ;
+
+
